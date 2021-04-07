d350293762b6eba731a13b246decf873a91a9e86
wagner luiz oliveira dos santos <wlsantos@cmb.gov.br>
2021-04-07 14:46:39 -0300

    incluir funcao useEffect e Memo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useEffect, useMemo, useState, useCallback} from 'react';
 
 function App() {
   const [tarefas,setTarefas] = useState([
@@ -7,10 +7,28 @@ function App() {
   ]);
   const [item,setItems] = useState('');
 
-  function handleAdd(){
-    console.log('adicionando');
+  useEffect(() =>{//chama qd monta a tela
+    console.log("carregou tela")
+    const tarefasStorange = localStorage.getItem('tarefas')
+    if(tarefasStorange){
+      setTarefas(JSON.parse(tarefasStorange))
+    }
+  },[])
+
+  useEffect(() =>{//chama qd modifica tarefas
+    console.log("mudou tarefas")
+    localStorage.setItem('tarefas',JSON.stringify(tarefas))
+  },[tarefas])
+
+  const handleAdd= useCallback(()=>{
+    console.log("handleAdd")
     setTarefas([...tarefas,item])
-  }
+    setItems('')
+  },[tarefas,item])
+  
+  // atualiza se houver alteracao no tarefas
+  const totaltarefas= useMemo(()=>tarefas.length,[tarefas]);
+
 
   return (
     <div className="App">
@@ -19,6 +37,7 @@ function App() {
           <li key={tarefa}>{tarefa}</li>
         ))}
       </ul>
+      <strong>Você tem {totaltarefas} tarefas</strong><br/>
       <input type="text" value={item} onChange={(e)=>setItems(e.target.value)}/>
       <button type="button" onClick={handleAdd}> adicionar</button>
     </div>
